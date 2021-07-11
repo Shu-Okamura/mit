18789e02f7b054e8cd313b458ee61d659421dbb2
shu_okamura <zou.shu.303@icluod.com>
2021-07-12 03:25:08 +0900

    first commit

---
+++ mit_spring_task/src/main/java/mit_spring_task/mit_spring_task/MitSpringTaskApplication.java
@@ -0,0 +1,13 @@
+package mit_spring_task.mit_spring_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitSpringTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitSpringTaskApplication.class, args);
+	}
+
+}
---
+++ mit_spring_task/src/test/java/mit_spring_task/mit_spring_task/MitSpringTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package mit_spring_task.mit_spring_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitSpringTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
